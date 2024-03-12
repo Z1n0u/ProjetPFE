14bd02eedf60cc8b8adc5fb8e27ab6534e87990b
Z1n0u <taztala1@gmail.com>
2024-03-12 15:42:37 +0100

    just added user model with identity framework so we can base our repos on this branch

---
+++ ProjetPFE.Server/Models/User.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProjetPFE.Server.Models
+{
+    public class User : IdentityUser
+    {
+        [Required]
+        [StringLength(100)]
+        public string Nom { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string Prenom { get; set; }
+        public DateOnly DateNaiss { get; set; }
+        public string Poste { get; set; }
+        public string Email { get; set; }
+        public string Adresse { get; set; }
+        [Required]
+        public int Matricule { get; set; }
+        public bool IsActive { get; set; } = false;
+
+    }
+}
--- ProjetPFE.Server/Program.cs
+++ ProjetPFE.Server/Program.cs
@@ -1,6 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using ProjetPFE.Server.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("tarekconnection") ?? throw new InvalidOperationException("Connection string not found.");
+
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connectionString));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
